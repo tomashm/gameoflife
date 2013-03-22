e290b5678139af888e9bfc76d7de5552cf47d05b
tommorte <tomas@finn.no>
2013-03-22 12:52:08 +0100

    init

---
+++ src/main/java/no/finntech/kata/App.java
@@ -0,0 +1,13 @@
+package no.finntech.kata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
